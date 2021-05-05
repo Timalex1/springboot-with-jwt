d6678355a3890aa582cec35e097a87690626a45c
Timilehin Vincent <timilehinvincent@gmail.com>
2021-05-05 02:03:42 +0100

    Auth for jwt Filter

--- src/main/java/com/thumbex/api/security/JwtAuthFilter.java
+++ src/main/java/com/thumbex/api/security/JwtAuthFilter.java
@@ -1,5 +1,33 @@
 package com.thumbex.api.security;
 
-public class JwtAuthFilter {
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class JwtAuthFilter extends GenericFilterBean {
+
+    private JwtAuthService authService;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        // TODO Auto-generated method stub
+
+        Authentication authentication = authService.getAuthentication((HttpServletRequest) request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+
+    }
 
 }
